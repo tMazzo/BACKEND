11eba83d15474270cbc467521e24791c0b93eb38
Tomás Mazzo <116767949+sirMazo@users.noreply.github.com>
2023-05-11 15:03:32 -0300

    CORS para auth

--- src/main/java/com/portafolio/TomasMazzzo/Security/Controller/AuthController.java
+++ src/main/java/com/portafolio/TomasMazzzo/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://bkd-portfolio-web.onrender.com")
 public class AuthController {
 
     @Autowired
